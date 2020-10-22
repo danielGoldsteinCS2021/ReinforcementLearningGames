022404a26c12bc28301a4444c857dbca5c963474
Daniel Goldstein <18djg@queensu.ca>
2020-10-21 21:28:43 -0400

    hard codded draft - does not work, same results as dnaiel1

--- two-playergames.py
+++ two-playergames.py
@@ -8,18 +8,36 @@ Part 2 - two player matrix games
 def gameAlgorithm(rewardMatrices, policies, episodes, alpha):
     numOfPlayers, numOfRows, numOfCols = len(policies), len(rewardMatricies[0]), len(rewardMatricies[1])  # each player has their own policy
     for _ in range(episodes):
-        for rowNum in range(numOfRows):  # each row corresponds to the player taking an action
-            for rewardIdx in range(numOfCols):
-                for player in range(numOfPlayers):
-                    rewardMatrix = rewardMatrices[player]  # get reward matrix for current player
-                    reward = rewardMatrix[rowNum][rewardIdx]
-                    policyForAction = policies[player][rowNum]
-                    policyForAction = policyForAction + alpha * reward * (1 - policyForAction)
-                    for rewardIdx2, reward2 in enumerate(rewardMatrix[rowNum]):  # for all other actions
-                        if rewardIdx2 == rewardIdx:  # action is the same as our current action
-                            continue
-                        policyForAction = policyForAction - alpha * reward2 * policyForAction
-                    policies[player][rowNum] = policyForAction
+
+        # action 1 for player 1
+        policies[0][0] = policies[0][0] + alpha * rewardMatricies[0][0][0] * (1 - policies[0][0])  # p2 a1
+        policies[0][0] = policies[0][0] + alpha * rewardMatricies[0][0][1] * (1 - policies[0][0])  # p2 a2
+
+        policies[0][1] = policies[0][1] - alpha * rewardMatricies[0][1][0] * policies[0][1]  # p2 a1
+        policies[0][1] = policies[0][1] - alpha * rewardMatricies[0][1][1] * policies[0][1]  # p2 a2
+
+        # action 1 for player 2
+        policies[1][0] = policies[1][0] + alpha * rewardMatricies[1][0][0] * (1 - policies[1][0])  # p2 a1
+        policies[1][0] = policies[1][0] + alpha * rewardMatricies[1][0][1] * (1 - policies[1][0])  # p2 a2
+
+        policies[1][1] = policies[1][1] - alpha * rewardMatricies[1][1][0] * policies[1][1]  # p2 a1
+        policies[1][1] = policies[1][1] - alpha * rewardMatricies[1][1][1] * policies[1][1]  # p2 a2
+        
+
+        # action 2 for player 1
+        policies[0][1] = policies[0][1] + alpha * rewardMatricies[0][1][0] * (1 - policies[0][1])  # p2 a1
+        policies[0][1] = policies[0][1] + alpha * rewardMatricies[0][1][1] * (1 - policies[0][1])  # p2 a2
+
+        policies[0][0] = policies[0][0] - alpha * rewardMatricies[0][0][0] * policies[0][0]  # p2 a1
+        policies[0][0] = policies[0][0] - alpha * rewardMatricies[0][0][1] * policies[0][0]  # p2 a2
+
+        # action 2 for player 2
+        policies[1][1] = policies[1][1] + alpha * rewardMatricies[1][1][0] * (1 - policies[1][1])  # p2 a1
+        policies[1][1] = policies[1][1] + alpha * rewardMatricies[1][1][1] * (1 - policies[1][1])  # p2 a2
+
+        policies[1][0] = policies[1][0] - alpha * rewardMatricies[1][0][0] * policies[1][0]  # p2 a1
+        policies[1][0] = policies[1][0] - alpha * rewardMatricies[1][0][1] * policies[1][0]  # p2 a2
+
     return policies
 
 

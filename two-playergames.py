24ce29c1b4b3e07381c2aa53a37ec9832d91b6c2
Daniel Goldstein <18djg@queensu.ca>
2020-10-21 22:44:53 -0400

    tried another forumla but it also does not work

--- two-playergames.py
+++ two-playergames.py
@@ -8,32 +8,27 @@ Part 2 - two player matrix games
 def gameAlgorithm(rewardMatrices, policies, episodes, alpha):
     numOfPlayers, numOfRows, numOfCols = len(policies), len(rewardMatricies[0]), len(rewardMatricies[1])  # each player has their own policy
     for _ in range(episodes):
-        # p1
-        policies[0][0] = policies[0][0] + alpha * rewardMatricies[0][0][0] * (1 - policies[0][0])
-        policies[0][0] = policies[0][0] - alpha * rewardMatricies[1][0][0] * policies[0][0]
+        for i in range(2):
+            for j in range(2):
+                policies[i][j] = policies[i][j] + alpha * rewardMatricies[i][0][0] * (1 - policies[i][j])
+                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][1][0] * policies[i][j]
+                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][0][1] * policies[i][j]
+                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][1][1] * policies[i][j]
 
-        policies[0][0] = policies[0][0] + alpha * rewardMatricies[0][0][1] * (1 - policies[0][0])
-        policies[0][0] = policies[0][0] - alpha * rewardMatricies[1][0][1] * policies[0][0]
+                policies[i][j] = policies[i][j] + alpha * rewardMatricies[i][1][0] * (1 - policies[i][j])
+                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][0][0] * policies[i][j]
+                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][0][1] * policies[i][j]
+                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][1][1] * policies[i][j]
 
-        policies[0][1] = policies[0][1] + alpha * rewardMatricies[0][1][0] * (1 - policies[0][1])
-        policies[0][1] = policies[0][1] - alpha * rewardMatricies[1][1][0] * policies[0][1]
+                policies[i][j] = policies[i][j] + alpha * rewardMatricies[i][0][1] * (1 - policies[i][j])
+                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][1][0] * policies[i][j]
+                policies[i][j] = policies[i][0] - alpha * rewardMatricies[i][0][0] * policies[i][j]
+                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][1][1] * policies[i][j]
 
-        policies[0][1] = policies[0][1] + alpha * rewardMatricies[0][1][1] * (1 - policies[0][1])
-        policies[0][1] = policies[0][1] - alpha * rewardMatricies[1][1][1] * policies[0][1]
-
-        # p2
-        policies[1][0] = policies[1][0] + alpha * rewardMatricies[1][0][0] * (1 - policies[1][0])
-        policies[1][0] = policies[1][0] - alpha * rewardMatricies[0][0][0] * policies[1][0]
-
-        policies[1][0] = policies[1][0] + alpha * rewardMatricies[1][0][1] * (1 - policies[1][0])
-        policies[1][0] = policies[1][0] - alpha * rewardMatricies[0][0][1] * policies[1][0]
-
-        policies[1][1] = policies[1][1] + alpha * rewardMatricies[1][1][0] * (1 - policies[1][1])
-        policies[1][1] = policies[1][1] - alpha * rewardMatricies[0][1][0] * policies[1][1]
-
-        policies[1][1] = policies[1][1] + alpha * rewardMatricies[1][1][1] * (1 - policies[1][1])
-        policies[1][1] = policies[1][1] - alpha * rewardMatricies[0][1][1] * policies[1][1]
-
+                policies[i][j] = policies[i][j] + alpha * rewardMatricies[i][1][1] * (1 - policies[i][j])
+                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][1][0] * policies[i][j]
+                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][0][1] * policies[i][j]
+                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][0][0] * policies[i][j]
     return policies
 
 

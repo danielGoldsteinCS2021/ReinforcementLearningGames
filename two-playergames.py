5696fd9ea8b592d852f0c01b3d76d037a24f4643
Daniel Goldstein <18djg@queensu.ca>
2020-10-21 22:59:13 -0400

    draft, I think it work's for single prison case, lot's of hard coding - needs to be abstracted

--- two-playergames.py
+++ two-playergames.py
@@ -6,35 +6,21 @@ Part 2 - two player matrix games
 
 
 def gameAlgorithm(rewardMatrices, policies, episodes, alpha):
-    numOfPlayers, numOfRows, numOfCols = len(policies), len(rewardMatricies[0]), len(rewardMatricies[1])  # each player has their own policy
     for _ in range(episodes):
         for i in range(2):
-            for j in range(2):
-                policies[i][j] = policies[i][j] + alpha * rewardMatricies[i][0][0] * (1 - policies[i][j])
-                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][1][0] * policies[i][j]
-                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][0][1] * policies[i][j]
-                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][1][1] * policies[i][j]
+            # player 1
+            policies[i][0] = policies[i][0] + alpha * (rewardMatrices[i][0][0] + rewardMatrices[i][0][1]) * (1 - policies[i][0])
+            policies[i][0] = policies[i][0] - alpha * (rewardMatrices[i][1][0] + rewardMatrices[i][1][1]) * policies[i][0]
 
-                policies[i][j] = policies[i][j] + alpha * rewardMatricies[i][1][0] * (1 - policies[i][j])
-                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][0][0] * policies[i][j]
-                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][0][1] * policies[i][j]
-                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][1][1] * policies[i][j]
-
-                policies[i][j] = policies[i][j] + alpha * rewardMatricies[i][0][1] * (1 - policies[i][j])
-                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][1][0] * policies[i][j]
-                policies[i][j] = policies[i][0] - alpha * rewardMatricies[i][0][0] * policies[i][j]
-                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][1][1] * policies[i][j]
-
-                policies[i][j] = policies[i][j] + alpha * rewardMatricies[i][1][1] * (1 - policies[i][j])
-                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][1][0] * policies[i][j]
-                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][0][1] * policies[i][j]
-                policies[i][j] = policies[i][j] - alpha * rewardMatricies[i][0][0] * policies[i][j]
+            # player 2
+            policies[i][1] = policies[i][1] + alpha * (rewardMatrices[i][1][0] + rewardMatrices[i][1][1]) * (1 - policies[i][1])
+            policies[i][1] = policies[i][1] - alpha * (rewardMatrices[i][0][1] + rewardMatrices[i][0][0]) * policies[i][1]
     return policies
 
 
 if __name__ == "__main__":
     player1, player2 = [[5, 0], [10, 1]], [[5, 10], [0, 1]]
     rewardMatricies = [player1, player2]
-    policies = [[0.5, 0.5], [0.5, 0.5]]
-    episodes = 50000
-    print(gameAlgorithm(rewardMatricies, policies, episodes, 0.001))
+    p = [[0.5, 0.5], [0.5, 0.5]]
+    e = 50000
+    print(gameAlgorithm(rewardMatricies, p, e, 0.001))
